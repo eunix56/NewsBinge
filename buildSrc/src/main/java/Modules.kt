4ec26476855f283464e10c1bdc31c2474458855e
Bolaji Alabi <alabibolaji78@gmail.com>
2024-02-27 11:18:42 +0100

    configured gradle for libraries to import from base gradle module

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,10 @@
+object Modules {
+
+    const val app = ":app"
+
+    const val home = ":features:home"
+    const val search =  ":features:search"
+    const val favourites = ":features:favourite"
+    const val settings = ":features:settings"
+    const val common = ":common"
+}
