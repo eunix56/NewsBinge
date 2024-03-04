c17441a36b4203b3f4d51fefbb83c867faf86438
Eunice B <eunice@reach.africa>
2024-03-04 22:01:18 +0100

    Renamed Color variables to get rid of Magic Number issue

--- app/src/main/java/com/app/newsbinge/ui/theme/Color.kt
+++ app/src/main/java/com/app/newsbinge/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.app.newsbinge.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Purple200N = Color(0xFFBB86FC)
+val Purple500N = Color(0xFF6200EE)
+val Purple700N = Color(0xFF3700B3)
+val Teal200N = Color(0xFF03DAC5)
