d044d1d31a62875f279aa8d1c885872c110415b3
Eunice B <eunice@reach.africa>
2024-03-04 22:17:16 +0100

    Renamed Color variables to get rid of Magic Number issue

--- app/src/main/java/com/app/newsbinge/ui/theme/Theme.kt
+++ app/src/main/java/com/app/newsbinge/ui/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200N,
-    primaryVariant = Purple700N,
-    secondary = Teal200N
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500N,
-    primaryVariant = Purple700N,
-    secondary = Teal200N
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
 
     /* Other default colors to override
     background = Color.White,
