4face644dc999aed64aea59f4241a95e911c7234
Eunice B <eunice@reach.africa>
2025-02-15 18:22:20 +0100

    Worked on animation

--- features/home/src/main/java/com/app/home/ui/HomeScreen.kt
+++ features/home/src/main/java/com/app/home/ui/HomeScreen.kt
@@ -5,10 +5,14 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -23,27 +27,49 @@ import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.onClick
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.lerp
+import androidx.compose.ui.window.Dialog
 import coil.compose.AsyncImage
 import com.app.common.model.NewsItemData
 import com.app.common.theme.Blue800
 import com.app.common.theme.Grey150
+import com.app.common.theme.NewsBingeTheme
 import com.app.home.R
 import kotlin.math.absoluteValue
 
@@ -64,14 +90,19 @@ fun HomeScreenContent() {
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun NewsCarousel(list: List<String>) {
+fun NewsCarousel(list: List<Int>) {
     val state = rememberPagerState(
         initialPage = 0,
         initialPageOffsetFraction = 0f
     ) { list.size }
 
-    HorizontalPager(state = state) {
+    Column {
+        HorizontalPager(state = state) {
+            Image(painter = painterResource(it),
+                contentDescription = null)
+        }
 
+        DotIndicators(state.pageCount, state.currentPage)
     }
 }
 
@@ -230,3 +261,17 @@ fun Modifier.carouselTransition(page: Int, pagerState: PagerState) =
         alpha = transformation
         scaleY = transformation
     }
+
+
+@Composable
+@Preview(showBackground = true)
+fun DefaultPreview() {
+    NewsBingeTheme {
+        NewsCarousel(
+            listOf(
+                R.drawable.news_placeholder,
+                R.drawable.author_placeholder
+            )
+        )
+    }
+}
